797dcc0626238e398d615fe2f384dd2e9598fe98
malik <malikhodrob99@gmail.com>
2023-07-10 21:01:50 +0300

    added stuff

---
+++ snacks/views.py
@@ -0,0 +1,9 @@
+# pylint: disable=import-error
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+class HomeView(TemplateView):
+    template_name= 'home.html'
+    
+class AboutView(TemplateView):
+    template_name= 'about.html'
